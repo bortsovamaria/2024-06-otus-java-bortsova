f49c317e78a80c1bedbdb8afe34e3027a0bae983
Maria Bortsova <bortsovamariia@gmail.com>
2024-08-17 13:35:52 +0300

    hw05-aop: refactor after review

--- HW05-aop/src/main/java/ru/aop/result/IoC.java
+++ HW05-aop/src/main/java/ru/aop/result/IoC.java
@@ -1,79 +1,71 @@
 package ru.aop.result;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 import static java.util.Objects.nonNull;
 
+@Slf4j
 class IoC {
-    private static final Logger logger = LoggerFactory.getLogger(IoC.class);
-
-    private static final List<List<String>> methods = getMethodsByAnnotation(Log.class);
 
     private IoC() {
     }
 
     static TestLoggingInterface createMyClass(TestLoggingInterface testLoggingClass) {
-        InvocationHandler handler = new CustomInvocationHandler(testLoggingClass);
+        CustomInvocationHandler handler = new CustomInvocationHandler(testLoggingClass);
+
         return (TestLoggingInterface) Proxy.newProxyInstance(
                 IoC.class.getClassLoader(), new Class<?>[]{TestLoggingInterface.class}, handler);
     }
 
     static class CustomInvocationHandler implements InvocationHandler {
         private final TestLoggingInterface myClass;
+        private final Set<MethodDTO> cacheMethods;
 
         CustomInvocationHandler(TestLoggingInterface myClass) {
             this.myClass = myClass;
+            this.cacheMethods = getMethodsByAnnotation(myClass.getClass(), Log.class);
         }
 
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-            if (isExistMethod(method)) {
-                logger.info("executed method from proxy: {}, param: {}", method, args);
+            MethodDTO currentMethod = getCurrentMethodDTO(method);
+
+            if (cacheMethods.contains(currentMethod)) {
+                log.info("executed method from proxy: {}, param: {}", method, args);
                 return method.invoke(myClass, args);
             }
             return method.invoke(myClass, args);
+        }
+
+        private Set<MethodDTO> getMethodsByAnnotation(Class<? extends TestLoggingInterface> clazz,
+                                                      Class<? extends Annotation> annotation) {
+            Method[] methods = clazz.getMethods();
+            Set<MethodDTO> methodsByAnnotation = new HashSet<>();
+            for (Method method : methods) {
+                if (nonNull(method.getAnnotation(annotation))) {
+                    methodsByAnnotation.add(getCurrentMethodDTO(method));
+                }
+            }
+            return methodsByAnnotation;
+        }
+
+        private MethodDTO getCurrentMethodDTO(Method method) {
+            return MethodDTO.builder()
+                    .name(method.getName())
+                    .parametersName(method.getParameterTypes())
+                    .build();
         }
 
         @Override
         public String toString() {
             return "CustomInvocationHandler{" + "myClass=" + myClass + '}';
         }
-    }
-
-    private static boolean isExistMethod(Method method) {
-        return methods.contains(getListParameterNamesByMethod(method));
-    }
-
-    private static List<List<String>> getMethodsByAnnotation(Class<? extends Annotation> annotation) {
-        Class<?> clazz;
-        try {
-            clazz = Class.forName(TestLoggingImpl.class.getName());
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException("Class not found");
-        }
-        Method[] methods = clazz.getMethods();
-        List<List<String>> result = new ArrayList<>();
-        for (Method method : methods) {
-            if (nonNull(method.getAnnotation(annotation))) {
-                List<String> list = getListParameterNamesByMethod(method);
-                result.add(list);
-            }
-        }
-        return result;
-    }
-
-    private static List<String> getListParameterNamesByMethod(Method method) {
-        return Arrays.stream(method.getParameters())
-                .map(m -> m.getParameterizedType().getTypeName())
-                .toList();
     }
 }
--- HW05-aop/src/main/java/ru/aop/result/TestLoggingImpl.java
+++ HW05-aop/src/main/java/ru/aop/result/TestLoggingImpl.java
@@ -1,33 +1,32 @@
 package ru.aop.result;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class TestLoggingImpl implements TestLoggingInterface {
-    private static final Logger logger = LoggerFactory.getLogger(TestLoggingImpl.class);
 
     @Log
     @Override
     public void calculation(int param1) {
-        logger.info("execute method calculation(int) from original class");
+        log.info("execute method calculation(int) from original class");
     }
 
     @Log
     @Override
     public void calculation(int param1, int param2) {
-        logger.info("execute method calculation(int, int) from original class");
+        log.info("execute method calculation(int, int) from original class");
     }
 
     @Log
     @Override
     public void calculation(int param1, int param2, int param3) {
-        logger.info("execute method calculation(int, int, int) from original class");
+        log.info("execute method calculation(int, int, int) from original class");
 
     }
 
     @Override
     public void calculation(String param) {
-        logger.info("execute method calculation(String) from original class");
+        log.info("execute method calculation(String) from original class");
 
     }
 
